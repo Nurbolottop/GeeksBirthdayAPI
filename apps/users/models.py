7a95edb5e8ed96f2c03ba3230f407311fc8a86d4
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-08-26 10:57:17 -0400

    new models

--- apps/users/models.py
+++ apps/users/models.py
@@ -15,6 +15,10 @@ class Users(models.Model):
         upload_to='image_users/',
         verbose_name="Фотография пользователя",
     )
+    user_level = models.CharField(
+        max_length=244,
+        verbose_name= "Уровень студента"
+    )
     
     def __str__(self):
         return f"Имя: {self.user_name} - Айди: {self.user_id}"
