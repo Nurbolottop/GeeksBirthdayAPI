4ff189ef66b425595860588961a44979390f41d3
Nurbolottop <erk1nbaevw2711@gmail.com>
2024-01-20 09:43:55 -0500

    Updatew

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
-from .models import Users
+from .models import Users,Gender
 # Register your models here.
 
 admin.site.register(Users)
+admin.site.register(Gender)
 admin.site.unregister(User)
 admin.site.unregister(Group) 
